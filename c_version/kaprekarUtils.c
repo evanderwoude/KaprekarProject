7b2f61a2d570dd171cd6047706d47449c1d3df98
evanderwoude <54680005+evanderwoude@users.noreply.github.com>
2019-09-05 21:38:56 -0500

    Update kaprekarUtils.c

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -1,57 +1,55 @@
+#include<stdlib.h>
+#include<stdio.h>
 #include<math.h>
 
 #include "kaprekarUtils.h"
 
-/**
- * <p>This function tests whether the given input <code>n</code>
- * is a Kaprekar Number or not.  A Kaprekar Number is a non-negative
- * integer n whose square can be split into two positive integers 
- * whose sum is equal to the original number.  For the purposes of 
- * this method, we restrict our consideration to base-10 representations.</p>
- * 
- * For example, 45 is a Kaprekar number, because 
- *   45<sup>2</sup> = 2025 
- * and 
- *   20 + 25 = 45. 
- * 
- * <p>See Wikipedia's  
- * <a href="https://en.wikipedia.org/wiki/Kaprekar_number">Kaprekar Number</a>
- * article for more information.</p>
- * 
- * @param n
- * @return <code>true</code> if <code>n</code> is a Kaprekar Number, 
- * <code>false</code> otherwise.
- */
 int isKaprekar(int n) {
 
   if(n < 1) {
     return 0;
   }
 
-  int i;
+  int i = 1;
   //compute the square
   long square = n*(long)n;
   //compute the number of digits of the square
-  int numDigits = (int)log10(n) + 1;
-  long modulus = 0;
+  int numDigits = (int)(log10(n) + 1);
+  long modulus = 1; //originally 0
   long first, second;
 
-  //for each possible "split" of the square...
-  for(i=1; i<=numDigits; i++) {
-    //increase the modulus by a factor of 10
-    modulus = 10;
+  //increase the modulus by a factor of 10
+  modulus = pow(10,numDigits);
 
-    //split the square into two parts
-    first = square / modulus;
-    second = square / modulus;
-
-    //test if the split makes a Kaprekar number
-    //the second part cannot be zero and if
-    //the sum of the two parts is equal to n, then it is
-    if(second > 0 && n == first + second) {
-      return 1;
+  //split the square into two parts
+  first = square/modulus;
+  second = (square-(first*modulus));
+  
+  //test if the split makes a Kaprekar number
+  //the second part cannot be zero and if
+  //the sum of the two parts is equal to n, then it is
+  if(second > 0 && n == first + second) {
+    return 1; //true
+  }
+  //to account for some numbers that have 
+  //zeros at the end of first number, divide by ten
+  else if(first % 10 == 0){
+    //some have more than 1 zero so you run until no more zeros
+    while(first % 10 == 0){
+      first /= 10;
+      second = (square-(first*modulus*pow(10,i)));
+      if(second > 0 && n == first + second) {
+        return 1; //true
+      //zero will cause infinit loop, so if zero it's false
+      }else if(first == 0){
+        return 0;
+      }
+      //account for dividing by ten again, 
+      //ups modulus by factor of 10
+      i++;
     }
   }
-  return 0;
+//}
+return 0;
   
 }
